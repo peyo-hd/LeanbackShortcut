c98a49ab66d7cb4ce2da45eb51f4c224d23ebd31
Peter Yoon <peyo.hd@gmail.com>
2016-01-14 21:41:20 +0900

    change to browser shortcut

--- app/src/main/java/com/peyo/leanback/shortcut/Browser.java
+++ app/src/main/java/com/peyo/leanback/shortcut/Browser.java
@@ -5,12 +5,12 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class Market extends Activity {
+public class Browser extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Intent intent = new Intent();
-		intent.setComponent(new ComponentName("com.android.vending", "com.android.vending.AssetBrowserActivity"));
+		intent.setComponent(new ComponentName("com.android.browser", "com.android.browser.BrowserActivity"));
 		startActivity(intent);
 		finish();
 	}
